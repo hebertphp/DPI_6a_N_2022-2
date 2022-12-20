ca4d2488278e2a963ac8efdefac947a5e50b9549
Prof. Hebert <hebertphp@gmail.com>
2022-12-19 21:46:47 -0300

    19/12/22

---
+++ aula91/script.js
@@ -0,0 +1,10 @@
+const mostrap=document.querySelector('.mostra-p');
+
+function mostra(){
+    if(mostrap.style.display=='none'){
+        mostrap.style.display='block';
+    }    
+    else{
+        mostrap.style.display='none';
+    }
+}
